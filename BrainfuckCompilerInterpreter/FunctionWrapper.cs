60efd9d4701231b1122ee9ef8e8b1d19b671dfac
ZorbaGyrka <empo@abv.bg>
2018-07-17 19:16:32 +0300

    Added FunctionWrapper.cs

---
+++ BrainfuckCompilerInterpreter/FunctionWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BrainfuckCompilerInterpreter.Compiler;
+using static BrainfuckCompilerInterpreter.Native;
+
+namespace BrainfuckCompilerInterpreter
+{
+    public unsafe class FunctionWrapper : IDisposable
+    {
+        internal IntPtr buf;
+        internal byte* bytePointer;
+        internal CompiledFuncType ptr;
+        protected virtual void Dispose(bool disposing)
+        {
+            VirtualFree(buf, UIntPtr.Zero, MemoryFreeType.MEM_RELEASE);
+            if (disposing)
+            {
+                // release other disposable objects  
+                // if (resource != null) resource.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public char* Invoke(char* input)
+        {
+            return ptr(input);
+        }
+    }
+}
